37249216bb7979b672fe3d38bd8b7a04f780bad5
icoloma <icoloma@gmail.com>
2014-08-17 20:45:58 +0200

    First hack at it

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,12 +2,12 @@
 
 if test ! $(which rbenv)
 then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+  echo "  Installing rbenv."
+  sudo apt-get -y install rbenv > /tmp/rbenv-install.log
 fi
 
 if test ! $(which ruby-build)
 then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+  echo "  Installing ruby-build."
+  sudo apt-get -y install ruby-build > /tmp/ruby-build-install.log
 fi
