a2fc9a8460201622b138628e1cb67503e9496a6f
Nacho Coloma <icoloma@gmail.com>
2014-09-07 10:01:54 +0200

    MIME default programs

---
+++ file-associations/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Configure default programs to open files
+set -e
+FOLDER=$(cd "$(dirname $0)"; pwd)
+
+if [ ! -f ~/.local/share/applications/mimeapps.list ]
+then
+  echo "   Configuring default programs to open mime types."
+  mkdir -p ~/.local/share/applications/
+  ln -s "$FOLDER/mimeapps.list" ~/.local/share/applications/mimeapps.list
+fi
