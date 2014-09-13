e8e030340e81a8e3811089d411b841c7aa163799
Nacho Coloma <icoloma@gmail.com>
2014-09-13 11:12:58 +0200

    Minor changes

--- file-associations/install.sh
+++ file-associations/install.sh
@@ -5,7 +5,7 @@ FOLDER=$(cd "$(dirname $0)"; pwd)
 
 if [ ! -f ~/.local/share/applications/mimeapps.list ]
 then
-  echo "   Configuring default programs to open mime types."
+  echo "   Configuring default applications associated to mime types."
   mkdir -p ~/.local/share/applications/
   ln -s "$FOLDER/mimeapps.list" ~/.local/share/applications/mimeapps.list
 fi
