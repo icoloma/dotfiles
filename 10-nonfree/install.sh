6100ca1dd4c27cdba6ee071acbc5d517506bd003
Nacho Coloma <icoloma@gmail.com>
2014-09-13 11:10:56 +0200

    Added Sublime Text Editor 2

--- 10-nonfree/install.sh
+++ 10-nonfree/install.sh
@@ -19,6 +19,14 @@ then
   google-chrome&
 fi
 
+# Install Sublime Text Editor 2
+# see http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3
+if test ! $(which subl) {
+	sudo add-apt-repository ppa:webupd8team/sublime-text-2
+	sudo apt-get update
+	sudo apt-get install sublime-text
+}
+
 # Skype does not support unbuntu 14 yet
 #if test ! $(which skype)
 #then
