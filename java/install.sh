4048f1887c96766618bc1aa1519ee6942e0c0960
Nacho Coloma <icoloma@gmail.com>
2014-09-16 10:40:45 +0200

    Added IntelliJ IDEA

--- java/install.sh
+++ java/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Google Cloud Platform requires Java 7. Feel free to upgrade to Java 8 if you are not using it
+# Select a concrete Java version with 
+# update-java-alternatives -s java-7-oracle
 if test ! $(which java)
 then
   echo "  Installing Java."
@@ -9,10 +11,20 @@ then
   sudo apt-get -y install oracle-java7-installer > /tmp/java-install.log
 fi
 
-
 if test ! $(which gradle)
 then
   sudo add-apt-repository ppa:cwchien/gradle
   sudo apt-get update
   sudo apt-get install gradle
 fi
+
+if [ ! -d /opt/idea ]
+then
+  URL=http://download-cf.jetbrains.com/idea/ideaIC-13.1.4b.tar.gz
+  FILENAME=$(echo $URL | grep -o 'idea[^/]\+')
+  echo "Installing IntelliJ IDEA into /opt/$(echo $FILENAME | sed s/\.tar\.gz//)"
+  wget -O /tmp/$FILENAME $URL
+  tar -xvzf /tmp/$FILENAME -C /tmp
+  sudo mv /tmp/idea*/ /opt
+fi
+
