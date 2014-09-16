09fe357086a96e6c01313a7016fc9b009f3d9e96
Nacho Coloma <icoloma@gmail.com>
2014-09-16 10:40:07 +0200

    Typo

--- 10-nonfree/install.sh
+++ 10-nonfree/install.sh
@@ -21,11 +21,12 @@ fi
 
 # Install Sublime Text Editor 2
 # see http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3
-if test ! $(which subl) {
+if test ! $(which subl)
+then
 	sudo add-apt-repository ppa:webupd8team/sublime-text-2
 	sudo apt-get update
 	sudo apt-get install sublime-text
-}
+fi
 
 # Skype does not support unbuntu 14 yet
 #if test ! $(which skype)
