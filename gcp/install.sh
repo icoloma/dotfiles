0b93c402178a93617025681ed7bc0e75c09a9670
Nacho Coloma <icoloma@gmail.com>
2014-09-30 18:09:49 +0200

    Corrected a TODO item for automatic components install

--- gcp/install.sh
+++ gcp/install.sh
@@ -5,7 +5,7 @@ if test ! $(which gcloud)
 then
   echo "  Installing Google Cloud SDK."
 # TODO: Make it install into /opt
-#  CLOUDSDK_CORE_DISABLE_PROMPTS=true
+# run install.sh --help to get a list of available flags
   curl https://sdk.cloud.google.com | bash
 
   # Install GAE python too
