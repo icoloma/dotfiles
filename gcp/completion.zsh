a32ccf799b2220c15d03be54487ca0cfd4ed21df
Nacho Coloma <icoloma@gmail.com>
2014-09-30 11:49:11 +0200

    Let gcp live in $HOME

--- gcp/completion.zsh
+++ gcp/completion.zsh
@@ -2,9 +2,9 @@
 # Copied from the GCP installed files
 
 # The next line updates PATH for the Google Cloud SDK.
-source '/opt/google-cloud-sdk/path.zsh.inc'
+source "$HOME/google-cloud-sdk/path.zsh.inc"
 
 # The next line enables bash completion for gcloud.
-source '/opt/google-cloud-sdk/completion.zsh.inc'
+source "$HOME/google-cloud-sdk/completion.zsh.inc"
 
-export APPENGINE_HOME=/opt/google-cloud-sdk/platform/appengine-java-sdk
+export APPENGINE_HOME=$HOME/google-cloud-sdk/platform/appengine-java-sdk
