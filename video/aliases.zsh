fe6490ad3bea2d2b9ec2ac92dcb16caee6cdfc6c
Nacho Coloma <icoloma@gmail.com>
2014-10-05 10:02:55 +0200

    Added openshot

--- video/aliases.zsh
+++ video/aliases.zsh
@@ -1,2 +1,2 @@
 # usage: record_video ~/Video/file.mp4
-alias record_video='avconv -strict experimental -v warning -f alsa -i default -f x11grab -r 15 -s wxga -i :0.0 -vcodec libx264 -preset ultrafast -threads auto -y -metadata title="Captured video" '
+alias record_video='avconv -v warning -f alsa -i default -f x11grab -r 15 -s wxga -i :0.0 -vcodec libx264 -preset ultrafast -threads auto -y -metadata title="Captured video" -strict experimental '
--- video/install.sh
+++ video/install.sh
@@ -3,5 +3,5 @@
 if test ! $(which avconv)
 then
   echo "  Installing video editing tools."
-  sudo apt-get -y install libav-tools
+  sudo apt-get -y install libav-tools openshot
 fi
