9e4f12d07c7220936106096e2e97af3109b19eae
Nacho Coloma <icoloma@gmail.com>
2014-10-08 21:39:39 +0200

    Added tools to convert video to gif

--- video/install.sh
+++ video/install.sh
@@ -3,5 +3,20 @@
 if test ! $(which avconv)
 then
   echo "  Installing video editing tools."
-  sudo apt-get -y install libav-tools openshot
+  sudo apt-get -y install libav-tools
+fi
+
+if test ! $(which gifsicle)
+then
+  sudo apt-get -y install gifsicle
+fi
+
+if test ! $(which openshot)
+then
+  sudo apt-get -y install openshot
+fi
+
+if test ! $(which pitivi)
+then
+  sudo apt-get -y install pitivi
 fi
