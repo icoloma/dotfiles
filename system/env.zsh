b70215cd43ac44754be0aa803e47bb455cc6a0bb
Nacho Coloma <icoloma@gmail.com>
2014-10-23 23:08:35 +0100

    Clean all references to brew Merge git aliases from https://github.com/garybernhardt

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,12 @@
-export EDITOR='atom'
+export EDITOR='subl'
+export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
+export GREP_OPTIONS="--color"
+
+
+# Nicer history
+export HISTSIZE=100000
+export HISTFILE="$HOME/.history"
+export SAVEHIST=$HISTSIZE
+
+# make ctrl + [ left | right ] work
+export WORDCHARS=''
