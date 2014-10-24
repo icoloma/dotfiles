13df2f1ff068a1f1dbc14895740a4edef3085872
Nacho Coloma <icoloma@gmail.com>
2014-10-24 13:52:41 +0200

    Minor changes

--- android/install.sh
+++ android/install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+set -e
 
 if test ! $(which adb)
 then
   echo "  Installing Android."
-  wget http://dl.google.com/android/android-sdk_r22.0.5-linux.tgz > /tmp/android-sdk-linux.tgz
+  sudo apt-get install android-tools-adb
+  wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz -O /tmp/android-sdk-linux.tgz
   tar -xvzf /tmp/android-sdk-linux.tgz -C /opt/
   . android/path.zsh
-  android update sdk --no-ui --filter 1
+  android update sdk --no-ui --filter 1,platform-tools
+  android&
 fi
