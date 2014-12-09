51cc8dadda2094c2e38dfd271cf3b03f3a27b0a8
Nacho Coloma <icoloma@gmail.com>
2014-12-09 10:11:15 +0100

    Added terminator config

--- terminator/install.sh
+++ terminator/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+FOLDER=$(cd "$(dirname $0)"; pwd)
 
 if test ! $(which terminator)
 then
   echo "  Installing terminator."
   sudo apt-get -y install terminator > /tmp/terminator-install.log
+  ln -s $FOLDER/config ~/.config/terminator/config
 fi
