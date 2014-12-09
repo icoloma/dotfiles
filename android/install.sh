493730fe81c37f423248cf3f31909eef2d6c1f1e
Nacho Coloma <icoloma@gmail.com>
2014-12-09 18:23:29 +0100

    Added missing libraries for aapt

--- android/install.sh
+++ android/install.sh
@@ -4,7 +4,7 @@ set -e
 if test ! $(which adb)
 then
   echo "  Installing Android."
-  sudo apt-get install libstdc++6
+  sudo apt-get install libstdc++6 lib32stdc++6
   wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz -O /tmp/android-sdk-linux.tgz
   tar -xvzf /tmp/android-sdk-linux.tgz -C /opt/
   . android/path.zsh
