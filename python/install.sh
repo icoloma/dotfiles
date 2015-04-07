396a2444cd2f9ac965ada1305794c2d9a73d100a
Nacho Coloma <icoloma@gmail.com>
2015-04-07 09:09:43 +0200

    Added l8 alias

--- python/install.sh
+++ python/install.sh
@@ -4,7 +4,7 @@
 if test ! $(which pip)
 then
 	echo "Installing Python tools"
-	sudo apt-get -y install python-pip python-dev build-essential
+	sudo apt-get -y install python-pip python-dev build-essential python3-pip
 	sudo pip install --upgrade pip 
 	sudo pip install --upgrade virtualenv 
 fi
@@ -18,7 +18,6 @@ fi
 # http://saurabh-kumar.com/blog/virtualenv-vs-virtualenvwrapper.html
 if test ! $(which virtualenv)
 then
-    echo "  Installing virtualenv"
+    echo "Installing virtualenv"
     sudo pip install -U virtualenv
-    virtualenv venv
 fi
