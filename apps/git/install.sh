9dcd186907c575865a05672d3f2ef7578ab4ad30
Nacho Coloma <icoloma@gmail.com>
2015-07-18 21:07:22 +0200

    Add git open

--- apps/git/install.sh
+++ apps/git/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Git and Ubuntu dependencies
 
-if test ! $(which meld)
-then
-  echo "Installing git tools"
+if test ! $(which meld); then
+  echo "Installing git gui and meld"
   sudo apt-get -y install git git-gui meld vim > /tmp/git-install.log
 fi
 
+if test $(which npm); then
+  echo "Installing git open"
+  sudo npm install -g git-open
+fi
