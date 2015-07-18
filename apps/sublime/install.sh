fc1be623b5bd603ed8213cfbba79ac6c41eca2fa
Nacho Coloma <icoloma@gmail.com>
2015-07-18 13:26:59 +0200

    Extracted macro outside of the install script, for easier maintenance

--- apps/sublime/install.sh
+++ apps/sublime/install.sh
@@ -7,8 +7,8 @@ source $FOLDER/../../_utils.sh
 
 # Install Sublime Text Editor 3 
 # see http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3
-if test ! $(which subl)
-then
+if test ! $(which subl); then
+	echo "Installing Sublime Text Editor 3"
 	sudo add-apt-repository ppa:webupd8team/sublime-text-3
 	sudo apt-get update
 	sudo apt-get install sublime-text-installer
@@ -16,13 +16,14 @@ fi
 
 mkdir -p "$HOME/.config/sublime-text-3/Installed Packages"
 mkdir -p "$HOME/.config/sublime-text-3/Packages/User"
-echo '[
-  {"command": "expand_selection", "args": {"to": "line"}},
-  {"command": "duplicate_line"}
-]' > "$HOME/.config/sublime-text-3/Packages/User/duplicate_lines.sublime-macro"
-ln -sf $FOLDER/Default\ \(Linux\).sublime-keymap ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
 
-# this should install all plugins required for a healthy and long life
-wget -O "$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" https://sublime.wbond.net/Package%20Control.sublime-package
+ln -sf "$FOLDER/duplicate_lines.sublime-macro" "$HOME/.config/sublime-text-3/Packages/User/duplicate_lines.sublime-macro"
+ln -sf "$FOLDER/Default (Linux).sublime-keymap" ~/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap
+
+# Install all plugins required for a healthy and long life
+
+SUBLIME_PACKAGE="$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package"
+[ -f "$SUBLIME_PACKAGE" ] || wget -O "$SUBLIME_PACKAGE" https://sublime.wbond.net/Package%20Control.sublime-package
+
 ln -sf "$FOLDER/Package Control.sublime-settings" ~/.config/sublime-text-3/Packages/User/
   
