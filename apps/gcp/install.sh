84426c6e0ea66b4abe8cfa054f64df7b3b2746f8
Nacho Coloma <icoloma@gmail.com>
2015-07-22 18:11:41 +0200

    Fix some GCP installation issues

--- apps/gcp/install.sh
+++ apps/gcp/install.sh
@@ -8,14 +8,29 @@ source $FOLDER/../../_utils.sh
 if test ! $(which gcloud)
 then
   echo "Installing Google Cloud Platform SDK"
-  
-  # TODO: Make it install into /opt
+
   # run install.sh --help to get a list of available flags
   curl https://sdk.cloud.google.com | bash
 
-  # Install some packages
-  source ~/.bash_profile
-  gcloud components update gae-python gae-java app-engine-java
+  if test ! "$(grep completion.bash.inc ~/.extra ~/.bashrc ~/.bash_profile)"; then
+
+    # the user did not let Google change .bashrc, we do it ourselves
+
+    ln -sf ~/google-cloud-sdk/bin/gcloud ~/bin
+    ln -sf ~/google-cloud-sdk/bin/gsutil ~/bin
+    ln -sf ~/google-cloud-sdk/bin/bq ~/bin
+
+    printf "\n# Google Cloud Platform Config\nsource $HOME/google-cloud-sdk/completion.bash.inc" >> ~/.extra
+
+    if test ! "$(grep 'alias gce' ~/.extra)"; then
+      echo "alias gce='gcloud compute'" >> ~/.extra
+    fi
+
+    printf '\n' >> ~/.extra
+
+  fi
+
+  gcloud components update app-engine-python app-engine-java app-engine-java
 fi
 
-extra_set APPENGINE_HOME=$HOME/google-cloud-sdk/platform/appengine-java-sdk
+extra_set GOOGLE_CLOUD_HOME=$HOME/google-cloud-sdk
