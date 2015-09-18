cb6dc827e679046e9d82cfea98d977e377cc0885
Nacho Coloma <icoloma@gmail.com>
2015-09-18 22:41:53 +0200

    Fix some issues with Android installation. Maybe

--- apps/android/install.sh
+++ apps/android/install.sh
@@ -11,11 +11,11 @@ then
   sudo apt-get install libstdc++6 lib32stdc++6
   wget http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz -O /tmp/android-sdk-linux.tgz
   tar -xvzf /tmp/android-sdk-linux.tgz -C /opt/
-  ln -s /opt/android-sdk-linux/tools/android $HOME/bin
-  android update sdk --no-ui --filter 1,platform-tools
-  android&
+  /opt/android-sdk-linux/tools/android update sdk --no-ui --filter 1,platform-tools
+  /opt/android-sdk-linux/tools/android&
 
-  ln -sf /opt/android-sdk-linux/platform-tools/adb ~/bin
+  ln -sf /opt/android-sdk-linux/tools/android $HOME/bin
+  ln -sf /opt/android-sdk-linux/platform-tools/adb $HOME/bin
 fi
 
 extra_set ANDROID_HOME=/opt/android-sdk-linux "Android tools"
