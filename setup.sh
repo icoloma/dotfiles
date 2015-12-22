e38a46d37437909216359cf536e37431315ec242
Nacho Coloma <icoloma@gmail.com>
2015-12-22 11:36:31 +0000

    Proper comment

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ read -n 1
 
 # We need more watches than what comes by default
 # https://confluence.jetbrains.com/display/IDEADEV/Inotify+Watches+Limit
-if test ! "$(grep fs.inotify.max_user_watches /etc/sysctl.conf)"; then
+# if test ! "$(grep fs.inotify.max_user_watches /etc/sysctl.conf)"; then
 #  echo 'Adding a bigger limit to max_user_watches'
 #  echo "fs.inotify.max_user_watches = 64768" | sudo tee -a '/etc/sysctl.conf' > /dev/null
 #  sudo sysctl -p
-fi
+# fi
 
