4fa1b961433589757870db54bec93d084a18ea3b
Nacho Coloma <icoloma@gmail.com>
2016-12-18 23:47:12 +0100

    Update the way of keeping node up-to-date

--- apps/javascript/install.sh
+++ apps/javascript/install.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 # node.js, Gulp, Grunt, etc
 
+# Official way of upgrading node and stuff
 # Install nvm, the node version manager
-# See https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
-#if test ! $(which nvm)
-#then
-#  sudo apt-get install build-essential libssl-dev
-#  curl https://raw.githubusercontent.com/creationix/nvm/v0.16.0/install.sh | bash
-#  source ~/.nvm/nvm.sh
-#  VERSION="$(nvm ls-remote | tail -1 | tr -d ' ')"
-#  echo "Installing node.js $VERSION"
-#  nvm install $VERSION
-#fi
-
-# test is missing here
-sudo apt-get -y install libcairo2-dev libjpeg-dev libgif-dev
-
-if test ! $(which node); then
-  curl -sL https://deb.nodesource.com/setup | sudo bash -
-
-  # Install n, manager for multiple versions of node
-  sudo npm install -g n
-  n latest
+# See https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+if test ! $(which nvm)
+then
+  sudo apt-get install build-essential libssl-dev
+  curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+  source ~/.nvm/nvm.sh
+  VERSION="$(nvm ls-remote | tail -1 | tr -d ' ')"
+  echo "Installing node.js $VERSION"
+  nvm install $VERSION
 fi
 
 if test ! $(which gulp); then
   echo "Installing gulp."
-  sudo npm -g install gulp
+  npm -g install gulp
 fi
 
-if test ! $(which grunt); then
-  echo "Installing grunt."
-  sudo npm -g install grunt-cli
-fi
-
-# if test ! $(which jshint); then
-#   echo "Installing JSHint."
-#   sudo npm -g install jshint jsxhint
-# fi
-
-if test ! $(which casperjs); then
-  echo "Installing CasperJS."
-  sudo npm -g install casperjs
-fi
