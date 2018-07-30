c5adf937afd1db09e364a21b9db677aeceefca77
Nacho Coloma <icoloma@gmail.com>
2018-07-30 07:00:25 +0100

    Add JS snippets

--- apps/VSCode/install.sh
+++ apps/VSCode/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 FOLDER=$(cd "$(dirname $0)"; pwd)
-mkdir -p ~/.config/Code/User
+mkdir -p ~/.config/Code/User/snippets
 ln -s "${FOLDER}/keybindings.json" "${FOLDER}/settings.json" ~/.config/Code/User
+ln -s "${FOLDER}/javascript.json" ~/.config/Code/User/snippets
