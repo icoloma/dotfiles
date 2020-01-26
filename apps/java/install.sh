8b2b7368682deea9730470b8ef817294dd1d62f9
Nacho Coloma <icoloma@gmail.com>
2020-01-26 23:59:40 +0100

    Update Java installation steps

--- apps/java/install.sh
+++ apps/java/install.sh
@@ -5,33 +5,20 @@ set -e
 FOLDER=$(cd "$(dirname $0)"; pwd)
 source $FOLDER/../../_utils.sh
 
-# Google Cloud Platform requires Java 7. Feel free to upgrade to Java 8 if you are not using it
 # Select a concrete Java version:
 # update-java-alternatives -s java-8-oracle
 if test ! $(which java); then
   echo "Installing Java"
-  sudo add-apt-repository ppa:webupd8team/java
-  sudo apt-get update > /tmp/java-install.log
-  sudo apt-get -y install oracle-java7-installer oracle-java8-installer oracle-java8-set-default
-  sudo update-java-alternatives -s java-8-oracle
+  sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
   source ~/.bash_profile
 fi
-extra_set JAVA_HOME=$(echo $(readlink -e $(which java)) | sed 's/oracle.*/oracle/')
 
 if test ! $(which gradle); then
-  sudo add-apt-repository ppa:cwchien/gradle
-  sudo apt-get update >> /tmp/java-install.log
+#  sudo add-apt-repository ppa:cwchien/gradle
   sudo apt-get -y install gradle
-  if [ ! -e ~/.gradle/gradle.properties ]; then
-    mkdir -p ~/.gradle
-    echo "# https://docs.gradle.org/2.5/userguide/gradle_daemon.html
-org.gradle.daemon=true" >> ~/.gradle/gradle.properties
-  fi
 fi
 
-if [ ! -d /opt/idea ]; then
-  echo "You have to install IntelliJ into /opt/idea yourself, I'm afraid."
-  echo "When prompted to import settings, use idea-settings.jar in this folder"
-  xdg-open https://www.jetbrains.com/idea/download/
+if test ! $(which idea); then
+  sudo snap install intellij-idea-community --classic
 fi
 
