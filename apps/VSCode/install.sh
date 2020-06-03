f48a3b5d623d49dbcda97d7eb47715d616c5a8f8
Nacho Coloma <icoloma@gmail.com>
2020-06-03 05:46:15 +0200

    Moved snippets to global snippets file, so they get applied on typescript too

--- apps/VSCode/install.sh
+++ apps/VSCode/install.sh
@@ -2,4 +2,4 @@
 FOLDER=$(cd "$(dirname $0)"; pwd)
 mkdir -p ~/.config/Code/User/snippets
 ln -s "${FOLDER}/keybindings.json" "${FOLDER}/settings.json" ~/.config/Code/User
-ln -s "${FOLDER}/javascript.json" ~/.config/Code/User/snippets
+ln -s "${FOLDER}/global.code-snippets" ~/.config/Code/User/snippets
