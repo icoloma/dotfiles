0b35fb0a1ed9efa4b52b59558f0da3d3c8726250
Nacho Coloma <icoloma@gmail.com>
2021-05-08 18:16:51 +0200

    Added fingerprint reader and LightDM

--- apps/fingerprint/install.sh
+++ apps/fingerprint/install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+# Fingerprint reader for Lenovo
+# Source: 
+# https://glsk.net/2018/05/t460s-fingerprint-reader-in-linux/
+# https://github.com/3v1n0/libfprint/blob/vfs0090/README.md#device-initialization-and-pairing
+
+# Initialize the device
+sudo snap install validity-sensors-tools
+sudo snap connect validity-sensors-tools:raw-usb
+sudo snap connect validity-sensors-tools:hardware-observe
+sudo validity-sensors-tools.initializer
+
+# Add the repository and install the tod package (supports both chips)
+sudo add-apt-repository -u ppa:3v1n0/libfprint-vfs0090
+sudo apt install libfprint-2-tod-vfs0090
+
+# Add to pam.d
 sudo apt install libpam-fprintd
-fprintd-enroll
+
+# enroll fingerprint
+fprintd-enroll -f "right-index-finger" "$USER"
+fprintd-verify
+
