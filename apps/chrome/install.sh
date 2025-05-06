4911f72c75ce0cab971c9bd91452aa04ef290ace
Nacho Coloma <icoloma@gmail.com>
2025-05-06 20:47:40 +0200

    Add steps to install and keep Chrome updated

--- apps/chrome/install.sh
+++ apps/chrome/install.sh
@@ -3,6 +3,10 @@
 
 if test ! $(which google-chrome)
 then
-  echo "Installing Google Chrome. You will have to do this by hand :)"
-  xdg-open http://www.google.com/chrome
+  # https://askubuntu.com/questions/1275967/how-to-update-chrome-in-ubuntu-20-04
+  echo "Installing Google Chrome"
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  sudo apt update 
+  sudo apt install google-chrome-stable
 fi
