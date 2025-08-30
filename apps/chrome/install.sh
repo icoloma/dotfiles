0e91be3815d56891f0b933795c4359bbf63f4c11
Nacho Coloma <icoloma@gmail.com>
2025-08-30 07:31:36 +0200

    Instructions changed, I guess?

--- apps/chrome/install.sh
+++ apps/chrome/install.sh
@@ -5,7 +5,7 @@ if test ! $(which google-chrome)
 then
   # https://askubuntu.com/questions/1275967/how-to-update-chrome-in-ubuntu-20-04
   echo "Installing Google Chrome"
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/chrome.gpg
   echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
   sudo apt update 
   sudo apt install google-chrome-stable
